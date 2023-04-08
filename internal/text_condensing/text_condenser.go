7668f7917d7a41e5c3befd12e999f5631360579e
Freddy Ouellette <freddy.ouellette@gmail.com>
2023-04-08 07:22:53 +0000

    fix condenser inaccuracy

--- internal/text_condensing/text_condenser.go
+++ internal/text_condensing/text_condenser.go
@@ -1,6 +1,10 @@
 package text_condensing
 
-import "github.com/freddyouellette/duolingo-text-extractor/internal/models"
+import (
+	"sort"
+
+	"github.com/freddyouellette/duolingo-text-extractor/internal/models"
+)
 
 // TextCondenser condenses texts so that each language only has one string.
 type TextCondenser struct{}
@@ -12,10 +16,12 @@ func NewTextCondenser() *TextCondenser {
 // Condense takes an array of Texts and condenses them, so that each language has only one string.
 // It will join each string with spaces.
 func (t *TextCondenser) Condense(texts []models.Text) ([]models.Text, error) {
+	languages := make([]string, 0)
 	languageTexts := make(map[string]string)
 	for _, text := range texts {
 		if _, ok := languageTexts[text.Language]; !ok {
 			languageTexts[text.Language] = ""
+			languages = append(languages, text.Language)
 		}
 		if len(languageTexts[text.Language]) > 0 {
 			languageTexts[text.Language] += " "
@@ -23,11 +29,12 @@ func (t *TextCondenser) Condense(texts []models.Text) ([]models.Text, error) {
 		languageTexts[text.Language] += text.Text
 	}
 
+	sort.Strings(languages)
 	var returnTexts []models.Text
-	for language, text := range languageTexts {
+	for _, language := range languages {
 		returnTexts = append(returnTexts, models.Text{
 			Language: language,
-			Text:     text,
+			Text:     languageTexts[language],
 		})
 	}
 
